7becac018f40a936acacc25cfbdabda534c8f87d
En Zhou <zhouen.nathan@gmail.com>
2020-10-14 22:21:59 +0200

    Update pgtools to create database

--- client.js
+++ client.js
@@ -1,22 +1,16 @@
-const { Client } = require('pg');
-
-const pgclient = new Client({
-    host: process.env.POSTGRES_HOST,
-    port: process.env.POSTGRES_PORT,
-    user: 'postgres',
-    password: 'postgres',
-    database: 'postgres'
-});
-
-pgclient.connect();
-
-const table = 'CREATE DATABASE "turf-portal"'
-// const table = 'CREATE TABLE student(id SERIAL PRIMARY KEY, firstName VARCHAR(40) NOT NULL, lastName VARCHAR(40) NOT NULL, age INT, address VARCHAR(80), email VARCHAR(40))'
-// const text = 'INSERT INTO student(firstname, lastname, age, address, email) VALUES($1, $2, $3, $4, $5) RETURNING *'
-// const values = ['Mona the', 'Octocat', 9, '88 Colin P Kelly Jr St, San Francisco, CA 94107, United States', 'octocat@github.com']
-
-pgclient.query(table, (err, res) => {
-    if (err) throw err
+var pgtools = require("pgtools");
+const config = {
+    user: "postgres",
+    host: "localhost",
+    password: "postgres",
+    port: 5432
+};
+pgtools.createdb(config, "turf-portal", function(err, res) {
+    if (err) {
+        console.error(err);
+        process.exit(-1);
+    }
+    console.log(res);
 });
 
 // pgclient.query(text, values, (err, res) => {
