4f794fd76efe368f1cff08fc76bfda8025798c74
En Zhou <zhouen.nathan@gmail.com>
2021-09-06 00:42:41 +0200

    Fix whitenoise in settings

--- turf_portal/settings.py
+++ turf_portal/settings.py
@@ -247,7 +247,7 @@ PROJECT_ROOT = os.path.join(os.path.abspath(__file__))
 # )
 
 #  Add configuration for static files storage using whitenoise
-STATICFILES_STORAGE = "whitenoise.django.CompressedManifestStaticFilesStorage"
+STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 
 prod_db = dj_database_url.config(conn_max_age=500)
