ab4266d6b14d5042d98151ecd9fc6ba313e36b4e
En Zhou <zhouen.nathan@gmail.com>
2023-09-02 17:30:07 -0600

    Update settings.py

--- turf_portal/settings.py
+++ turf_portal/settings.py
@@ -203,7 +203,8 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [
     BASE_DIR / "turf_portal" / "static",
 ]
-STATIC_ROOT = BASE_DIR / "static"
+# STATIC_ROOT = BASE_DIR / "static"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 DEFAULT_ROLL_WIDTH = 2
 DEFAULT_ROLL_HEIGHT = 30
