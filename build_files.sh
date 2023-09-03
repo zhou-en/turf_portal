3ab35d6c31ff2be36fdd7448ad797bd10647fdd2
En Zhou <zhouen.nathan@gmail.com>
2023-09-03 11:31:20 -0600

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -2,6 +2,7 @@
 pip install -r requirements.txt
 
 # make migrations
+python3.9 manage.py makemigrations
 python3.9 manage.py migrate 
 python3.9 manage.py collectstatic
 # python3.9 manage.py createsuperuser --noinput
