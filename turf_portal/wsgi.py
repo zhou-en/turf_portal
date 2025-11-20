d5ab371fa03c6b34c7923d19ff9eeedd2ef6bada
En Zhou <ezhou@vendasta.com>
2025-11-20 16:03:05 -0600

    fix: Restore missing get_wsgi_application import in wsgi.py

--- turf_portal/wsgi.py
+++ turf_portal/wsgi.py
@@ -7,5 +7,11 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'turf_portal.settings')
+
 application = get_wsgi_application()
 app = application
