369f3680349ee61bc4bd141f71b8c76821e427f1
En Zhou <ezhou@vendasta.com>
2025-11-20 15:59:19 -0600

    fix: Revert manual WhiteNoise wrapper in wsgi.py to fix local development

--- turf_portal/wsgi.py
+++ turf_portal/wsgi.py
@@ -7,15 +7,5 @@ For more information on this file, see
 https://docs.djangoproject.com/en/3.1/howto/deployment/wsgi/
 """
 
-import os
-
-from django.core.wsgi import get_wsgi_application
-from whitenoise import WhiteNoise
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'turf_portal.settings')
-
 application = get_wsgi_application()
-# Explicitly wrap with WhiteNoise to serve static files
-# pointing to the 'staticfiles' directory at project root
-application = WhiteNoise(application, root=os.path.join(os.path.dirname(os.path.dirname(__file__)), 'staticfiles_build', 'static'), prefix='static/')
 app = application
